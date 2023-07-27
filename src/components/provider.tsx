61d624245ce9c1ccfd87a6324b253b49dac2e388
giovani.franz <giovani.franz@agi.com.br>
2023-07-27 19:17:34 -0300

    refact: provider

--- src/components/provider.tsx
+++ src/components/provider.tsx
@@ -6,12 +6,12 @@ import * as React from 'react'
 
 import { Analytics } from '@vercel/analytics/react'
 
-import { isMockEnabled } from '@/lib/utils'
+import { isDevEnvironment, isMockEnabled } from '@/lib/utils'
 
 export function Provider({ children }: PropsWithChildren) {
   const [shouldRender, setShouldRender] = useState(false)
   const reportAccessibility = async () => {
-    if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
+    if (typeof window !== 'undefined' && isDevEnvironment()) {
       const axe = await import('@axe-core/react')
       const ReactDOM = await import('react-dom')
 
@@ -29,8 +29,7 @@ export function Provider({ children }: PropsWithChildren) {
   }
 
   useEffect(() => {
-    reportAccessibility()
-    initMocks()
+    Promise.all([reportAccessibility(), initMocks()])
   }, [])
 
   if (!shouldRender) return null
